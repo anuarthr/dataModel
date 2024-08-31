d1fc0298b4f29058764d6bc2e5e29d31e9374f43
anuarthr <anuarthrincongonzalez@gmail.com>
2024-08-31 13:49:21 -0500

    initial commit

---
+++ src/main/java/com/data/tallermodelodatos/TallerModeloDatosApplication.java
@@ -0,0 +1,13 @@
+package com.data.tallermodelodatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerModeloDatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerModeloDatosApplication.class, args);
+    }
+
+}
